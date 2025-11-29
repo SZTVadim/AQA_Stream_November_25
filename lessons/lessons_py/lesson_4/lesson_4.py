d5650c9a3880b2b86541fc023811233f6be691f4
v.savin <v.savin@insitech.dev>
2025-11-30 00:54:00 +0300

    First commit

---
+++ lessons/lessons_py/lesson_4/lesson_4.py
@@ -0,0 +1,72 @@
+# a = [1,2,3,4,5]
+# b = a
+# b.append(6)
+# c = [1,2,3,4,5]
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# print(a)
+#
+#
+# x = True
+# print(type(x))
+# x = '10'
+# print(type(x))
+# x = 10.0
+# print(type(x))
+
+# a = 3 + 4
+# print(a)
+# b = 5 ** 3
+# print(b)
+# c = 170 // 14
+# print(c)
+# d = 12 * 14
+# print(d)
+# e = 170 % 14
+# print(e)
+#
+# print(type(e < d))
+# print(e < d)
+
+# a = 5
+# b = 10 / 2
+
+# print(a == b)
+# print(a != b)
+
+# x = 1
+# print(x)
+# x += 3  # x = x + 3
+# print(x)
+#
+# x = 1
+# print(x)
+# x -= 3  # x = x - 3
+# print(x)
+
+a = [1,2,3,4,5]
+# print(33 in a)
+# print(3 in a)
+# print(33 not in a)
+# print(3 not in a)
+
+# print(3 and 6 in a)
+# print(7 in a or 6 in a)
+print(bool('1'))  # gherfier hf iuweh
+
+if 5 > 3:
+    print('Круто')
+
+
+def methon():
+    '''
+    wef wef e
+    wef we f
+    wef wef we
+    fwe fwef
+    wef ghjhghjghghjghjghjghj g uygfjut ftyj fytf ytujf tuygf ug
+    ugf kuguhkghugkig ihg lih ih ihg ukyg ulig ilh li gljgli hghgvkuyg kuigy8g giyl giyg ihbvhguyugffgufghghgjvgjgjjhjg
+     glgyilgfyigyigiygf fukyfkufukyf
+    '''
+    a = 1
